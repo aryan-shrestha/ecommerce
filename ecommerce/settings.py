917e67141e3d460dc724a6db9f21f110337292f4
aryan-shrestha <aryanshrestha183@gmail.com>
2021-05-23 20:33:39 +0545

    password removed from settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -134,4 +134,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'readitbyaryan@gmail.com'
-EMAIL_HOST_PASSWORD = 'Dhulabari@183'
+EMAIL_HOST_PASSWORD = ''
