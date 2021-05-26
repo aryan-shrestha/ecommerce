733c7aab40524d6869c242258322627b309cd034
aryan-shrestha <aryanshrestha183@gmail.com>
2021-05-26 18:47:17 +0545

    generic views used in API

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('item/', views.item_list),
-    path('item/<int:pk>/', views.item_detail),
-    path('brand/', views.brand_list),
-    path('brand/<int:pk>/', views.brand_detail),
-    path('category/', views.category_list),
-    path('category/<int:pk>/', views.category_detail),
-
+    path('item/', views.ItemList.as_view()),
+    path('item/<int:pk>/', views.ItemDetail.as_view()),
+    path('brand/', views.BrandList.as_view()),
+    path('brand/<int:pk>/', views.BrandDetail.as_view()),
+    path('category/', views.CategoryList.as_view()),
+    path('category/<int:pk>/', views.CategoryDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- api/views.py
+++ api/views.py
@@ -1,145 +1,29 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from rest_framework import status
+from rest_framework import generics
 
 from home.models import Item, Brand, Category
 from api.serializers import ItemSerializer, BrandSerializer, CategorySerializer
 
 # Create your views here.
+class ItemList(generics.ListCreateAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
 
-@csrf_exempt
-@api_view(['GET', 'POST'])
-def item_list(request):
-    '''
-    List all Items or create a new Item
-    '''
-    if request.method == 'GET':
-        items = Item.objects.all()
-        serializer = ItemSerializer(items, many=True)
-        return Response(serializer.data)
+class ItemDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
 
-    elif request.method == 'POST':
-        serializer = ItemSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class BrandList(generics.ListCreateAPIView):
+   queryset = Brand.objects.all()
+   serializer_class = BrandSerializer
 
-@csrf_exempt
-@api_view(['GET', 'PUT', 'DELETE'])
-def item_detail(request, pk):
-    '''
-    Retrieve, update or delete a item
-    '''
-    try:
-        item = Item.objects.get(pk=pk)
-    except Item.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+class BrandDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Brand.objects.all()
+    serializer_class = BrandSerializer
 
-    if request.method == 'GET':
-        serializer = ItemSerializer(item)
-        return Response(serializer.data)
+class CategoryList(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
 
-    elif request.method == 'PUT':
-        serializer = ItemSerializer(item, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        item.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-@csrf_exempt
-@api_view(['GET', 'POST'])
-def brand_list(request):
-    '''
-        List all Brand or create a new Brand
-    '''
-    if request.method == 'GET':
-        brands = Brand.objects.all()
-        serializer = BrandSerializer(brands, many=True)
-        return Response(serializer.data)
-
-    elif request.method == 'POST':
-        serializer = BrandSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@csrf_exempt
-@api_view(['GET', 'PUT', 'DELETE'])
-def brand_detail(request, pk):
-    '''
-    Retrieve, update or delete a brand
-    '''
-    try:
-        brand = Brand.objects.get(pk=pk)
-    except Brand.DoesNotExist:
-        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = BrandSerializer(brand)
-        return Response(serializer.data)
-
-    elif request.method == 'PUT':
-        serializer = BrandSerializer(brand, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        brand.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-@csrf_exempt
-@api_view(['GET', 'POST'])
-def category_list(request):
-    '''
-        list all the categories or create a categories
-    '''
-
-    if request.method == 'GET':
-        categories = Category.objects.all()
-        serializer = CategorySerializer(categories, many=True)
-        return Response(serializer.data)
-
-    elif request.method == 'POST':
-        serializer = CategorySerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@csrf_exempt
-@api_view(['GET', 'PUT', 'DELETE'])
-def category_detail(request, pk):
-    '''
-    Retrieve, update or delete a brand
-    '''
-    try:
-        category = Category.objects.get(pk=pk)
-    except Brand.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = CategorySerializer(category)
-        return Response(serializer.data)
-
-    elif request.method == 'PUT':
-        serializer = CategorySerializer(category, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        category.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
